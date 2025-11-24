8a70b149f244c45c9b619c675c13d161c79088f3
Adarsh Kumar <kadarsh.singh99@gmail.com>
2025-11-25 03:09:42 +0530

    Add bridging header for Swift-Objective-C interoperability

---
+++ ios/StoreAgeDeclaration-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  StoreAgeDeclaration-Bridging-Header.h
+//  React Native Store Age Declaration
+//
+//  Bridging header for Swift-Objective-C interoperability
+//
+
+#import <React/RCTBridgeModule.h>
